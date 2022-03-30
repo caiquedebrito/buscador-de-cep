5188fd43e4d09ff08a40484a2362c0af015e5d2e
Caique De Brito <caiquedebritoo@gmail.com>
2022-03-30 10:50:01 -0300

    arquivo _redirects criado

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <Buscador />
-      <div className="link"><a href="https://suporte.boxloja.pro/article/90-faixa-de-ceps-do-brasil" target="_blank" rel="external">Faixas de ceps</a></div>
+      <div className="link"><a href="https://suporte.boxloja.pro/article/90-faixa-de-ceps-do-brasil" target="_blank" rel="noreferrer">Faixas de ceps</a></div>
     </div>
   );
 }
